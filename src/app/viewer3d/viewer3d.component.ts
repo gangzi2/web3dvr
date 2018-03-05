8c0851eda8ee9ced66ecd06b317244a9db22573a
gangzi3 <Chenggang_WANG@symetrix.fr>
2018-03-05 16:15:53 +0100

    changes

--- src/app/viewer3d/viewer3d.component.ts
+++ src/app/viewer3d/viewer3d.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit ,ElementRef,ViewChild} from '@angular/core';
 import * as THREE from 'three';
+import { Vector3 } from 'three';
 @Component({
   selector: 'app-viewer3d',
   templateUrl: './viewer3d.component.html',
@@ -21,17 +22,16 @@ export class Viewer3dComponent implements OnInit {
    }
 
   ngOnInit() {
-    this.container = this.elementRef.nativeElement;
+    this.container =  document.getElementById('3dviewer');
     console.log(this.container);
-
     this.init();
- 
   }
   init(){
     let screen = {
-      width  : 400,
-      height : 300
+      width  : 500,
+      height : 400
     },
+    
     view = {
       angle  : 45,
       aspect : screen.width / screen.height,
@@ -42,44 +42,46 @@ export class Viewer3dComponent implements OnInit {
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera(view.angle, view.aspect, view. near, view.far);
     this.renderer = new THREE.WebGLRenderer();
-
     this.scene.add(this.camera);
     this.scene.add(new THREE.AxesHelper(20));
-    
+    this.scene.background = new THREE.Color( 0xbfd1e5 );
+
     this.camera.position.set(10,10,10);
     this.camera.lookAt(new THREE.Vector3(0,0,0));
-
     this.renderer.setSize(screen.width, screen.height);
     this.container.appendChild(this.renderer.domElement);
-
-
     let geometry = new THREE.BoxGeometry(5, 5, 5),
-        material = new THREE.MeshBasicMaterial({ color : 0xFFFFFF, wireframe: true });
-        
+        material = new THREE.MeshLambertMaterial({ color : 0x00FFFF, overdraw: 0.5});
+
     this.cube = new THREE.Mesh( geometry, material );
-    this.cube.position.set(-50,-50,-50);
-    
+ 
+     
+    var ambientLight = new THREE.AmbientLight( 0xcccccc );
+    this.scene.add( ambientLight );
+
+    var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
+				directionalLight.position.x = Math.random() - 0.5;
+				directionalLight.position.y = Math.random() - 0.5;
+				directionalLight.position.z = Math.random() - 0.5;
+				directionalLight.position.normalize();
+				this.scene.add( directionalLight );
     this.scene.add(this.cube);
     
     this.render();
   }
   render(){
-
     let self: Viewer3dComponent = this;
-    
     (function render(){
       requestAnimationFrame(render);
       self.renderer.render(self.scene, self.camera);
-
       self.animate();
     }());
-    
   }
 
   animate(){
+  
     this.cube.rotateX(0.1);
     this.cube.rotateY(0.1);
-    this.cube.position.addScalar(0.2);
   }
 
 }
